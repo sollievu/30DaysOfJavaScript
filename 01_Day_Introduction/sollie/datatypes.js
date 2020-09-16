fa63cc8a5108a483876f0babbe9cb45f1070b388
sollievu <vungochoaithuong29@gmail.com>
2020-09-16 21:44:50 +0300

    creating datatypes.js

---
+++ 01_Day_Introduction/sollie/datatypes.js
@@ -0,0 +1,30 @@
+console.log(typeof "Sollie") 
+console.log(typeof 10) 
+console.log(typeof false) 
+console.log(typeof null) 
+console.log(typeof undefined)
+var a;
+var b;
+var c;
+var d;
+
+a = "sollie";
+b = undefined;
+c = 58;
+d = false;
+
+//Declare four variables with assigned values
+var first_name = "Sollie"; var last_name = "Vu"; var marital_status = "single"; var country = "Vietnam"; var age = 28;
+
+var first_name ="Son"; 
+var last_name = "Vu"; 
+var marital_status = "marriage"; 
+var country = "Vietnam"; 
+var age = 20;
+
+var myAge = 27;
+var yourAge = 20;
+console.log(myAge);
+console.log(yourAge);
+
+
