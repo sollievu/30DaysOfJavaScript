8cb2f67090b5276bba6a4f22398816da43227e01
sollievu <vungochoaithuong29@gmail.com>
2020-09-16 20:52:55 +0300

    creating variable.js

---
+++ variable.js
@@ -0,0 +1,10 @@
+//comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/*comments can make code readable, 
+easy to reuse and informative
+*/
+
+var a = "Sollie Vu";
+var b = true;
+var c = undefined;
+var d = null;
