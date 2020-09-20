3aa8fcddfe1af2e68eb1f7da20eb07c686ec59f6
sollievu <vungochoaithuong29@gmail.com>
2020-09-20 14:05:01 +0300

    finish assignment

---
+++ 05_Day_Arrays/sollie5/assignment5.js
@@ -0,0 +1,20 @@
+
+//ass1
+let text = 'I love teaching and empowering people, I teach HTML,  CSS,JS, React, Python';
+let arr = text.split(' ');
+console.log(arr);
+
+//ass2 
+let shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+shoppingCart.unshift('Meat') //adding Meat to the beginning of the array
+shoppingCart.pop('Honey');//remove Honey to the beginning of the array
+shoppingCart.push('Sugar'); //adding Sugar to the end of the array
+console.log(shoppingCart);
+
+shoppingCart[2] = 'Green Tea'; //modify Tea to 'Green Tea'
+console.log(shoppingCart);
+
+
+
+
+
