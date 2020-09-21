3859e226e0831c4fb5bb61a70b0aee20ab06141c
sollievu <vungochoaithuong29@gmail.com>
2020-09-21 16:28:03 +0300

    adding assignment 5

--- 05_Day_Arrays/sollie5/assignment5.js
+++ 05_Day_Arrays/sollie5/assignment5.js
@@ -14,7 +14,33 @@ console.log(shoppingCart);
 shoppingCart[2] = 'Green Tea'; //modify Tea to 'Green Tea'
 console.log(shoppingCart);
 
+/*In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. 
+If it does not exist add to the countries list.*/
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'];
+if (countries.includes('Ethiopia')){
+    console.log('ETHIOPIA');
+}else{
+    countries.push('Ethiopia');
+    console.log(countries);
+}
 
 
+/*In the webTechs array check if Sass exists in the array and if it exists 
+print 'Sass is a CSS preprocess'. 
+If it does not exist add Sass to the array and print the array.*/
 
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] 
+if (webTechs.includes('Sass')){
+    console.log('Sass is a CSS preprocess');
+}else{
+    webTechs.push('Sass');
+    console.log(webTechs);
+}
 
+/*Concatenate the following 
+two variables and store it in a fullStack variables*/
+
+let frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+let backEnd = ['Node','Express', 'MongoDB'];
+let fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
