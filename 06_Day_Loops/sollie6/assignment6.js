2081ea5356f107b0321219858f98a839d18e9919
sollievu <vungochoaithuong29@gmail.com>
2020-09-23 16:20:57 +0300

    [Assignment6] completing assignment

--- 06_Day_Loops/sollie6/assignment6.js
+++ 06_Day_Loops/sollie6/assignment6.js
@@ -101,4 +101,63 @@ do {
   const mernStack = ['MongoDB', 'Express', 'React', 'Node']
 
 
+//Write a loop that makes the following pattern using console.log():
+/*  #
+    ##
+    ###
+    ####
+    #####
+    ######
+    ####### 
+    */
+
+    for (let i = '#'; i.length <= 7; i=i+"#"){
+      console.log(i);
+    }
+
+ /*Use loop to print the following pattern:
+
+0 x 0 = 0
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
+4 x 4 = 16
+5 x 5 = 25
+6 x 6 = 36
+7 x 7 = 49
+8 x 8 = 64
+9 x 9 = 81
+10 x 10 = 100 */ 
+
+
+let i=0;
+for(i >= 0; i< 11; i++){
+  console.log(i, ' x ', i, ' = ', i*i);
+}
+
+
+
+/*Use for loop to iterate 
+from 0 to 100 and print the sum of all evens and the sum of all odds.
+
+The sum of all evens 
+from 0 to 100 is 2550. And the sum of all odds from 0 to 100 is 2500 */
+let sumOddNumbers = 0;
+let sumEvenNumbers = 0;
+for (let j = 0; j <= 100 ; j++){
+  if (j % 2 ==0){
+    sumEvenNumbers += j;
+    console.log(sumEvenNumbers);
+  }else{
+    sumOddNumbers += j;
+    console.log(sumOddNumbers);
+  }
+}
+sumEvenNumbers;
+sumOddNumbers;
+
+// Develop a small script which generate array of 5 random numbers
+
+
+
 
